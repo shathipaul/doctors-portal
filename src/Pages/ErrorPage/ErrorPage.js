a238b106187d69b6082dfb199208de1c7d4b217e
Shathi paul <108425880+shathipaul@users.noreply.github.com>
2023-02-24 00:06:46 +0600

    added error page

---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Server doesn't work</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Router/Router/Router.js
+++ src/Router/Router/Router.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Appointment from "../../Pages/Appointment/Appointment";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Main />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
